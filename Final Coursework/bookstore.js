fb8c2246e67be9bc6f98734a059c32013eb84fbd
Monica Manyanga <monicatapiwa3@gmail.com>
2024-10-30 19:12:39 +0400

    first commit

---
+++ Final Coursework/bookstore.js
@@ -0,0 +1,94 @@
+let bookstore = [
+
+    {
+          "image": "images/dance.jpg", 
+          "subject" : "Ballet",
+          "Location": "London",
+          "Price": 100,
+          "Spaces": 5,
+          "rating": 5 
+
+    },
+    {
+        "image": "images/Swim.jpg",
+        "subject" : "Swim",
+        "Location": "London",
+        "Price": 100,
+        "Spaces": 5,
+        "rating": 5 
+
+    },
+    {
+        "image": "images/Soccer.jpg",
+        "subject" : "Soccer",
+        "Location": "York",
+        "Price": 80,
+        "Spaces": 5,
+        "rating": 5 
+
+    },
+    {
+        "image": "images/Music.webp ",
+        "subject" : "Music",
+        "Location": "Bristol",
+        "Price": 90,
+        "Spaces": 5,
+        "rating": 4 
+
+    },
+    {
+        "image": "images/Sign1.jpg ",
+        "subject" : "Sign Language for baby",
+        "Location": "Botswana",
+        "Price": 50,
+        "Spaces": 5,
+        "rating": 5 
+
+    },
+    {
+        "image": "images/creative.jpg",
+        "subject" : "Creavite Minds",
+        "Location": "New York",
+        "Price": 280,
+        "Spaces": 5,
+        "rating": 5 
+
+    },
+    {
+        "image": "images/base.jpg",
+        "subject" : "Baseball",
+        "Location": "Zimbabwe",
+        "Price": 190,
+        "Spaces": 5,
+        "rating": 4 
+
+    },
+    {
+        "image": "images/gym.jpg ",
+        "subject" : "Gym",
+        "Location": "Brazil",
+        "Price": 150,
+        "Spaces": 5,
+        "rating": 5 
+
+    },
+    {
+        "image": "images/netball.webp",
+        "subject" : "Netball",
+        "Location": "Germarny",
+        "Price": 20,
+        "Spaces": 5,
+        "rating": 4 
+
+    },
+    {
+        "image": "images/guiter.webp",
+        "subject" : "Guiter",
+        "Location": "Kenya",
+        "Price": 24,
+        "Spaces": 5,
+        "rating": 5 
+
+    },
+
+];
